6b65b4a70b674f5d2853027f195304a2cbb83000
PedroGonçaloCorreia <goncalerta@gmail.com>
2021-05-05 18:01:02 +0100

    HorizontalLineDrawerTest

--- src/test/java/gui/drawer/entity/HouseDrawerTest.java
+++ src/test/java/gui/drawer/entity/HouseDrawerTest.java
@@ -6,6 +6,7 @@ import model.Position;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
 class HouseDrawerTest {
