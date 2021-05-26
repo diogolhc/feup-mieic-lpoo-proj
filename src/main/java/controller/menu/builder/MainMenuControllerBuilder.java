3edd48406d5e4585be2e65a3d1dc15613cb8de13
diogolhc <64315652+diogolhc@users.noreply.github.com>
2021-05-26 16:31:14 +0100

    Implements initial not complete version of pause menu

--- src/main/java/controller/menu/builder/MainMenuControllerBuilder.java
+++ src/main/java/controller/menu/builder/MainMenuControllerBuilder.java
@@ -24,17 +24,6 @@ public class MainMenuControllerBuilder extends MenuControllerBuilder {
         this.height = height;
     }
 
-    public MainMenuController buildMenu() {
-        Menu menu = new Menu(this.getTitle(), new Position(0,0), this.getWidth(), this.getHeight());
-
-        MainMenuController mainMenuController = this.getMenuController(menu);
-        for (ButtonController buttonController: this.getButtons()) {
-            mainMenuController.addButton(buttonController);
-        }
-
-        return mainMenuController;
-    }
-
     @Override
     protected List<ButtonController> getButtons() {
         List<ButtonController> buttons = super.getButtons();
