e7c2ea5e6adda4f9e4bb394f316a9aeb98589f56
diogolhc <64315652+diogolhc@users.noreply.github.com>
2021-05-28 19:53:45 +0100

    AnimalViewer test

---
+++ src/test/java/viewer/farm/element/entity/AnimalViewerTest.java
@@ -0,0 +1,172 @@
+package viewer.farm.element.entity;
+
+import gui.Color;
+import gui.GUI;
+import model.Position;
+import model.farm.entity.Animal;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+
+class AnimalViewerTest {
+    private GUI gui;
+    private Color backgroundColors[][] = new Color[10][10];
+    private Color foregroundColors[][] = new Color[10][10];
+    private char characters[][] = new char[10][10];
+    private Color currentBackgroundColor = Color.BLACK;
+    private Color currentForegroundColor = Color.WHITE;
+    private Animal animal;
+    
+
+    @BeforeEach
+    void setUp() {
+        this.gui = Mockito.mock(GUI.class);
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                this.backgroundColors[i][j] = this.currentBackgroundColor;
+                this.foregroundColors[i][j] = this.currentForegroundColor;
+                this.characters[i][j] = ' ';
+            }
+        }
+
+        Mockito.doAnswer(invocation -> {
+            this.currentBackgroundColor = invocation.getArgument(0);
+            return null;
+        }).when(gui).setBackgroundColor(Mockito.any());
+
+        Mockito.doAnswer(invocation -> {
+            this.currentForegroundColor = invocation.getArgument(0);
+            return null;
+        }).when(gui).setForegroundColor(Mockito.any());
+
+        Mockito.doAnswer(invocation -> {
+            int x = invocation.getArgument(0);
+            int y = invocation.getArgument(1);
+            char c = invocation.getArgument(2);
+            this.backgroundColors[y][x] = currentBackgroundColor;
+            this.foregroundColors[y][x] = currentForegroundColor;
+            this.characters[y][x] = c;
+            return null;
+        }).when(gui).drawChar(Mockito.anyInt(), Mockito.anyInt(), Mockito.anyChar());
+
+        Mockito.doAnswer(invocation -> {
+            String s = invocation.getArgument(2);
+            int x = invocation.getArgument(0);
+            int y = invocation.getArgument(1);
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                this.backgroundColors[y][x] = currentBackgroundColor;
+                this.foregroundColors[y][x] = currentForegroundColor;
+                this.characters[y][x] = c;
+                x += 1;
+            }
+            return null;
+        }).when(gui).drawString(Mockito.anyInt(), Mockito.anyInt(), Mockito.anyString());
+
+        Mockito.doAnswer(invocation -> {
+            int x = invocation.getArgument(0);
+            int y = invocation.getArgument(1);
+            return this.backgroundColors[y][x];
+        }).when(gui).getBackgroundColor(Mockito.anyInt(), Mockito.anyInt());
+
+        Mockito.doAnswer(invocation -> {
+            int x = invocation.getArgument(0);
+            int y = invocation.getArgument(1);
+            return this.foregroundColors[y][x];
+        }).when(gui).getForegroundColor(Mockito.anyInt(), Mockito.anyInt());
+    }
+
+    @Test
+    void drawSingle() {
+        Color WHITE = Color.WHITE;
+        Color BLACK = Color.BLACK;
+        Color ANIMAL = new Color("#012345");
+        this.animal = Mockito.mock(Animal.class);
+        Mockito.when(this.animal.getPosition()).thenReturn(new Position(2,1));
+
+
+        AnimalViewer viewer = new AnimalViewer();
+        viewer.draw(this.animal, 'A', ANIMAL, this.gui);
+
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (i == 2 && j == 1) {
+                    Assertions.assertEquals('A', this.characters[j][i]);
+                    Assertions.assertEquals(ANIMAL, this.foregroundColors[j][i]);
+                } else {
+                    Assertions.assertEquals(' ', this.characters[j][i]);
+                    Assertions.assertEquals(WHITE, this.foregroundColors[j][i]);
+                }
+                Assertions.assertEquals(BLACK, this.backgroundColors[j][i]);
+            }
+        }
+    }
+
+    @Test
+    void drawSeveral() {
+        Color BLACK = Color.BLACK;
+        Color WHITE = Color.WHITE;
+        Color ANIMAL = new Color("#012345");
+        this.animal = Mockito.mock(Animal.class);
+
+        AnimalViewer viewer = new AnimalViewer();
+        Mockito.when(this.animal.getPosition()).thenReturn(new Position(2,1));
+        viewer.draw(this.animal, 'A', ANIMAL, this.gui);
+        Mockito.when(this.animal.getPosition()).thenReturn(new Position(4,2));
+        viewer.draw(this.animal, 'B', ANIMAL, this.gui);
+        Mockito.when(this.animal.getPosition()).thenReturn(new Position(5,3));
+        viewer.draw(this.animal, 'C', ANIMAL, this.gui);
+
+
+        Color expectedBg[][] = {
+                {BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK},
+                {BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK},
+                {BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK},
+                {BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK},
+                {BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK},
+                {BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK},
+                {BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK},
+                {BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK},
+                {BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK},
+                {BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK},
+        };
+
+        Color expectedFg[][] = {
+                {WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE},
+                {WHITE, WHITE, ANIMAL, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE},
+                {WHITE, WHITE, WHITE, WHITE, ANIMAL, WHITE, WHITE, WHITE, WHITE, WHITE},
+                {WHITE, WHITE, WHITE, WHITE, WHITE, ANIMAL, WHITE, WHITE, WHITE, WHITE},
+                {WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE},
+                {WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE},
+                {WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE},
+                {WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE},
+                {WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE},
+                {WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE},
+        };
+
+        char expectedChars[][] = {
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', 'A', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', 'B', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', 'C', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        };
+
+        for (int i = 0; i < 10; i++) {
+            Assertions.assertArrayEquals(expectedBg[i], this.backgroundColors[i]);
+            Assertions.assertArrayEquals(expectedFg[i], this.foregroundColors[i]);
+            Assertions.assertArrayEquals(expectedChars[i], this.characters[i]);
+        }
+
+    }
+
+}
