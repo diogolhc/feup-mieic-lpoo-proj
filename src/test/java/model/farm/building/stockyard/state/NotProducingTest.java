8e28ab5b0f4065fc726af62963c8d7d018519c9d
diogolhc <64315652+diogolhc@users.noreply.github.com>
2021-05-29 16:32:09 +0100

    Stockyard states tests

---
+++ src/test/java/model/farm/building/stockyard/state/NotProducingTest.java
@@ -0,0 +1,27 @@
+package model.farm.building.stockyard.state;
+
+import model.InGameTime;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class NotProducingTest {
+    private NotProducing state;
+    private final InGameTime zero = new InGameTime(0);
+
+    @BeforeEach
+    void setUp() {
+        state = new NotProducing();
+    }
+
+    @Test
+    void setRemainingTimeIsNoOp() {
+        Assertions.assertEquals(zero, state.getRemainingTime());
+        state.setRemainingTime(new InGameTime(20));
+        Assertions.assertEquals(zero, state.getRemainingTime());
+        state.setRemainingTime(new InGameTime(0));
+        Assertions.assertEquals(zero, state.getRemainingTime());
+    }
+
+}
